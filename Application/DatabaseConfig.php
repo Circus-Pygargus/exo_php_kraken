ce3294651dbf54692f35b593df236e9a9e8d17f0
Circus-Pygargus <richard.meuret>
2020-10-10 09:12:16 +0000

    mise en place architecture MVC : DatabaseConfig

---
+++ Application/DatabaseConfig.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Application;
+
+
+use Dotenv\Dotenv;
+
+
+class DatabaseConfig
+{
+
+    /**
+     * @var PDO
+     */
+    public $db;
+
+    private function config ()
+    {
+        // load phpdotenv
+        $dotenv = Dotenv::createImmutable($_SERVER["DOCUMENT_ROOT"] . "/src");
+
+        $dotenv->load();
+        
+        try {
+            $this->db = new \PDO('mysql:host=' . $_ENV['HOSTNAME'] . ';dbname=' . $_ENV['DBNAME'], $_ENV['USER'], $_ENV['PASSWORD'], [\PDO::ATTR_EMULATE_PREPARES=>false]);
+        }
+        catch (\Exception $e) {
+            die('Erreur : ' . $e->get->message());
+        }
+    }
+
+    public function connect ()
+    {
+        $this->config();
+    }
+}
