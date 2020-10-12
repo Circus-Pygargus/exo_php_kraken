36a92406600e7b12a32d1b6a4f5248e8ebbf78e8
Circus-Pygargus <richard.meuret>
2020-10-12 18:40:16 +0000

    ajout gestion back de la route /kraken/create

--- src/Controller/KrakenController.php
+++ src/Controller/KrakenController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 
 use App\Application\Controller;
 
+use App\Entity\Kraken;
+
 
 class KrakenController extends Controller
 {
@@ -25,5 +27,60 @@ class KrakenController extends Controller
     public function new ()
     {
         return $this->twig->render('kraken_new/index.html.twig');
+    }
+
+
+    /**
+     * route '/kraken/create'
+     * 
+     * user sent the kraken creation form
+     */
+    public function create ()
+    {
+        $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+
+        if ($contentType === 'Application/json') {
+            // get and decode new kraken form content
+            $content = trim(file_get_contents("php://input"));
+            $decoded = json_decode($content, true);
+            // get form content as variables
+            extract($decoded);
+
+            $error = [];
+
+            !$name ? $error["name"] = "Il faut entrer un nom" : $name = htmlspecialchars($name);
+            
+            !$age ? $error["age"] = "Il faut entrer un age" : $age = (int) $age;
+
+            !$height ? $error["height"] = "Il faut entrer une taille" : $height = (int) $height;
+
+            !$weight ? $error["weight"] = "Il faut entrer un poids" : $weight = (int) $weight;
+
+            if (count($error) !== 0) {
+                $response = array(
+                    'response' => 'not ok',
+                    'errors' => $error, 
+                    'errorMessage' => 'Il y a des erreurs dans le formulaire'
+                );
+            }
+            else {
+                $krakenModel = new Kraken();
+                $isKrakenSaved = $krakenModel->add($name, $age, $height, $weight);
+
+                if (!$isKrakenSaved) {
+                    $response = array(
+                        'response' => 'not ok',
+                        'errorMessage' => 'Il y a eu un problème pendant l\'enregistrement en base de données'
+                    );
+                }
+                else {
+                    $response = array(
+                        'reponse' => 'ok',
+                        'message' => 'Kraken enregistré !'
+                    );
+                }
+            }
+            echo json_encode($response);
+        }
     }
 }
