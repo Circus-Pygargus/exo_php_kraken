9499b210460a30ea6ee98ad5de3648f09daa2b38
Circus-Pygargus <richard.meuret@laposte.net>
2020-10-14 14:07:00 +0000

    ajout gestion front-end pour la route /tentacle

--- public/js/tentacle-create.js
+++ public/js/tentacle-create.js
@@ -1,11 +1,45 @@
 // the form
-// const tentacleCreateForm = document.querySelector('#tentacle-create-form');
+const tentacleCreateForm = document.querySelector('#tentacle-create-form');
 const tentacleNameInput = document.querySelector('#tentacle-name');
 const tentacleLifePointsInput = document.querySelector('#tentacle-life-points');
 const tentacleForceInput = document.querySelector('#tentacle-force');
 const tentacleDexterityInput = document.querySelector('#tentacle-dexterity');
 const tentacleConstitutionInput = document.querySelector('#tentacle-constitution');
 
+// places to display infos
+const tentacleInfosDiv = document.querySelector('#tentacles-infos');
+console.log(tentacleInfosDiv);
 
+tentacleCreateForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = {
+        "krakenId": tentacleCreateForm.dataset.krakenId,
+        "name": tentacleNameInput.value,
+        "lifePoints": tentacleLifePointsInput.value,
+        "force": tentacleForceInput.value,
+        "dexterity": tentacleDexterityInput.value,
+        "constitution": tentacleDexterityInput.value
+    };
+
+    fetch('/tentacle', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "Application/json"
+        }
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(response => {
+        if (response.errors) {
+            // todo gérer les erreurs
+        }
+        else {
+            tentacleInfosDiv.innerHTML = response.tentacleHtml;
+        }
+    })
+});
 
 
