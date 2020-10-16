a4c3d7b4a2da82123ecc8118fba5ba2926b3e983
Circus-Pygargus <richard.meuret@laposte.net>
2020-10-16 04:35:06 +0000

    modif : route suppression tentacule en /tentacle/delete

--- public/index.php
+++ public/index.php
@@ -25,8 +25,7 @@ $router->map('GET', '/kraken/[i:krakenId]', array('c' => 'KrakenController', 'a'
 //user wants to create a tentacle
 $router->map('POST', '/tentacle', array('c' => 'TentacleController', 'a' => 'create'));
 // user wants to remove a tentacle
-$router->addMatchTypes(array('tentacleId' => '[0-9]{1,5}'));
-$router->map('POST', '/tentacle/[i:tentacleId]', array('c' => 'TentacleController', 'a' => 'delete'));
+$router->map('POST', '/tentacle/delete', array('c' => 'TentacleController', 'a' => 'delete'));
 
 
 
