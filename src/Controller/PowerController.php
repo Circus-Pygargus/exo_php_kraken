e91cc738d1582f52dbfa0912ca9c8b834f9f1464
Circus-Pygargus <richard.meuret@laposte.net>
2020-10-17 21:07:05 +0000

    ajout gestion coté back de la route '/power/remove'

--- src/Controller/PowerController.php
+++ src/Controller/PowerController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Application\Controller;
 
+use App\Entity\Kraken;
 use App\Entity\Power;
 use App\Entity\KrakenPower;
 use App\Entity\Tentacle;
@@ -60,7 +61,7 @@ class PowerController extends Controller
                     if (!$isPowerRecorded) {
                         $response = array(
                             'response' => 'not ok',
-                            'errorMessage' => 'Il y a eu un problème en BDD durant l\enregistrement de ce pouvoir'
+                            'errorMessage' => 'Il y a eu un problème en BDD durant l\'enregistrement de ce pouvoir'
                         );
                     }
                     else {
@@ -70,21 +71,122 @@ class PowerController extends Controller
                         $allPowers = $powerModel->getAll();
                         $availablePowers = $this->getAvailablePowers($allPowers, $krakenPowers);
 
+                        $krakenModel = new Kraken();
+                        $kraken = $krakenModel->getOneById($krakenId);
+
                         $powersHtml = $this->twig->render('partial/powers-infos.html.twig', [
                             "krakenPowers" => $krakenPowers
                         ]);
 
                         $addablePowersHtml = $this->twig->render('partial/powers-add.html.twig', [
+                            "kraken" => $kraken,
                             "availablePowers" => $availablePowers
                         ]);
 
                         $removablePowersHtml = $this->twig->render('partial/powers-delete.html.twig', [
+                            "kraken" => $kraken,
                             "krakenPowers" => $krakenPowers
                         ]);
 
                         $response = array(
                             'response' => 'ok',
                             'message' => 'Pouvoir enregistré',
+                            'powersHtml' => $powersHtml,
+                            'addablePowersHtml' => $addablePowersHtml,
+                            'removablePowersHtml' => $removablePowersHtml
+                        );
+                    }
+                }
+            }
+            echo json_encode($response);
+        }
+    }
+
+
+    /**
+     * Remove a power to a kraken
+     * 
+     * Route '/power/remove'
+     */
+    public function remove ()
+    {
+        $contentType = isset($_SERVER["CONTENT_TYPE"]) ? $_SERVER["CONTENT_TYPE"] : '';
+        
+        if ($contentType === "Application/json") {
+            $content = trim(file_get_contents("php://input"));
+            $decoded = json_decode($content, true);
+
+            extract($decoded);
+
+            $errors = [];
+
+            !$krakenId ? $errors["krakenId"] = 'Il manque l\'D du kraken' : $krakenId = (int) $krakenId;
+
+            !$powerId ? $errors["powerId"] = 'Il manque l\'ID du kraken' : $powerId = (int) $powerId;
+
+            if (count($errors) !== 0) {
+                $response = [
+                    'response' => 'not ok',
+                    'errors' => $errors,
+                    'errorMessage' => 'Il y a un problème avec le formulaire'
+                ];
+            }
+            else {
+                $tentacleModel = new Tentacle();
+                $tentaclesNb = $tentacleModel->getTentaclesNb($krakenId)["COUNT(id)"];
+
+                $krakenPowerModel = new KrakenPower();
+                $krakenPowersNb = $krakenPowerModel->getPowersNb($krakenId)["COUNT(id)"];
+                
+                if ($krakenPowersNb === 1) {
+                    $response = array(
+                        'response' => 'not ok',
+                        'errorMessage' => 'Impossible de supprimer le pouvoir de base du kraken'
+                    );
+                }
+                else if (($tentaclesNb >= 4 && $tentaclesNb < 8 && $krakenPowersNb === 2) ||
+                    ($tentaclesNb === 8 && $krakenPowersNb === 3))
+                {
+                    $response = array(
+                        'response' => 'not ok',
+                        'errorMessage' => 'Impossible de supprimer un pouvoir, il faut supprimer au moins un tentacule'
+                    );
+                }
+                else {
+                    $isPowerdeleted = $krakenPowerModel->delete($powerId);
+                    if (!$isPowerdeleted) {
+                        $response = array(
+                            'response' => 'not ok',
+                            'errorMessage' => 'Il y a eu un problème en BDD durant l\'enregistrement de ce pouvoir'
+                        );
+                    }
+                    else {
+                        $krakenPowers = $krakenPowerModel->getAllPowersByKrakenId($krakenId);
+
+                        $powerModel = new Power();
+                        $allPowers = $powerModel->getAll();
+                        $availablePowers = $this->getAvailablePowers($allPowers, $krakenPowers);
+
+                        $krakenModel = new Kraken();
+                        $kraken = $krakenModel->getOneById($krakenId);
+
+                        $powersHtml = $this->twig->render('partial/powers-infos.html.twig', [
+                            "krakenPowers" => $krakenPowers
+                        ]);
+
+                        $addablePowersHtml = $this->twig->render('partial/powers-add.html.twig', [
+                            "kraken" => $kraken,
+                            "availablePowers" => $availablePowers
+                        ]);
+
+                        $removablePowersHtml = $this->twig->render('partial/powers-delete.html.twig', [
+                            "kraken" => $kraken,
+                            "krakenPowers" => $krakenPowers
+                        ]);
+
+                        $response = array(
+                            'response' => 'ok',
+                            'message' => 'Pouvoir retiré',
                             'powersHtml' => $powersHtml,
                             'addablePowersHtml' => $addablePowersHtml,
                             'removablePowersHtml' => $removablePowersHtml
