e73bfb315d7419eb9e4b87e0aa502b4a81119859
Circus-Pygargus <richard.meuret@laposte.net>
2020-10-17 13:47:11 +0000

    ajout gestion coté back de la route '/power/add'

---
+++ src/Controller/PowerController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Controller;
+
+use App\Application\Controller;
+
+use App\Entity\Power;
+use App\Entity\KrakenPower;
+use App\Entity\Tentacle;
+
+
+class PowerController extends Controller
+{
+    /**
+     * Route '/power/add'
+     * 
+     * Add a power
+     */
+    public function add ()
+    {
+        $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+
+        if ($contentType === "Application/json") {
+            $content = trim(file_get_contents("php://input"));
+            $decoded = json_decode($content, true);
+            extract($decoded);
+            
+            $errors = [];
+
+            !$krakenId ? $errors["krakenId"] = 'Il manque l\'ID du kraken' : $krakenId = (int) $krakenId;
+
+            !$powerId ? $errors["powerId"] = 'Il manque l\'ID du pouvoir' : $powerId = (int) $powerId;
+
+            if (count($errors) !== 0) {
+                $response = array(
+                    'response' => 'not ok',
+                    'errors' => $errors,
+                    'errorMessage' => 'Il y a des erreurs dans le formulaire'
+                );
+            }
+            else {
+                $tentacleModel = new Tentacle();
+                $tentaclesNb = $tentacleModel->getTentaclesNb($krakenId)["COUNT(id)"];
+
+                $krakenPowerModel = new KrakenPower();
+                $krakenPowersNb = $krakenPowerModel->getPowersNb($krakenId)["COUNT(id)"];
+                
+                if (($tentaclesNb < 4 && $krakenPowersNb === 1) ||
+                    ($tentaclesNb >= 4 && $tentaclesNb < 8 && $krakenPowersNb === 2) ||
+                    ($tentaclesNb === 8 && $krakenPowersNb === 3))
+                {
+                    $response = array(
+                        'response' => 'not ok',
+                        'errorMessage' => 'Impossible d\'ajouter un pouvoir vu le nombre de tentacules'
+                    );
+                }
+                else {
+                    $isPowerRecorded = $krakenPowerModel->add($krakenId, $powerId);
+
+                    if (!$isPowerRecorded) {
+                        $response = array(
+                            'response' => 'not ok',
+                            'errorMessage' => 'Il y a eu un problème en BDD durant l\enregistrement de ce pouvoir'
+                        );
+                    }
+                    else {
+                        $krakenPowers = $krakenPowerModel->getAllPowersByKrakenId($krakenId);
+
+                        $powerModel = new Power();
+                        $allPowers = $powerModel->getAll();
+                        $availablePowers = $this->getAvailablePowers($allPowers, $krakenPowers);
+
+                        $powersHtml = $this->twig->render('partial/powers-infos.html.twig', [
+                            "krakenPowers" => $krakenPowers
+                        ]);
+
+                        $addablePowersHtml = $this->twig->render('partial/powers-add.html.twig', [
+                            "availablePowers" => $availablePowers
+                        ]);
+
+                        $removablePowersHtml = $this->twig->render('partial/powers-delete.html.twig', [
+                            "krakenPowers" => $krakenPowers
+                        ]);
+
+                        $response = array(
+                            'response' => 'ok',
+                            'message' => 'Pouvoir enregistré',
+                            'powersHtml' => $powersHtml,
+                            'addablePowersHtml' => $addablePowersHtml,
+                            'removablePowersHtml' => $removablePowersHtml
+                        );
+                    }
+                }
+            }
+            echo json_encode($response);
+        }
+    }
+
+
+
+    /**
+     * Get available powers for a kraken
+     * 
+     * @param array $allPowers All existing powers
+     * @param array $krakenPowers The kraken powers
+     * 
+     * @return array
+     */
+    private function getAvailablePowers ($allPowers, $krakenPowers): array
+    {
+        $availablePowers = [];
+
+        foreach ($allPowers as $possiblePower) {
+            $powerSeen = false;
+            foreach ($krakenPowers as $krakenPower) {
+                if ($possiblePower["id"] === $krakenPower["power_id"]) $powerSeen = true;
+            }
+
+            if (!$powerSeen) array_push($availablePowers, $possiblePower);
+        }
+
+        return $availablePowers;
+    }
+}
